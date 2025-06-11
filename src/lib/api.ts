f7d7cfa8449a05cdee10a2dd940aabf18c570ac5
Shaneshwar Bhagat <49476737+Shaneshwarbhagat@users.noreply.github.com>
2025-06-11 16:59:31 +0530

    added netlify proxy file to get API call

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios"
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://8.218.174.70:3000/api"
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? '/api' : "http://8.218.174.70:3000/api"
 
 console.log("API_BASE_URL:", API_BASE_URL)
+console.log("NODE_ENV:", process.env.NODE_ENV)
 
 const api = axios.create({
   baseURL: API_BASE_URL,
